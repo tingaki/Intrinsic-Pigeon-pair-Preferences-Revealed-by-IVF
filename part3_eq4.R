04ef9bbb52bef1d8105a1a0a3b2dca57089d5c83
tingaki <51158729+tingaki@users.noreply.github.com>
2024-05-21 15:30:54 +0800

    Update part3_eq4.R

--- part3_eq4.R
+++ part3_eq4.R
@@ -9,7 +9,7 @@
 # Part 1: Compilation of raw data (in STATA).
 # Part 2: Estimation of the proportion of delivering mothers who adopt IVF in the mother group indexed by age (a) and family wealth-rank (w) in period (t) using Nonlinear Least Squares (NLS) regression (in STATA).
 # Part 3: Estimation of the gender preference parameters of first-birth mothers who adopt IVFs (using Maximum Likelihood Estimation, MLE, in R).
-# Part 4: Calculation of the MLE standard errors of parameters and the values used in Figures 3(a) and (b) based on the estimates obtained in Part 3 (in STATA). 
+# Part 4: Calculation of the MLE standard errors of parameters and the values used in Figures 3 and 4 based on the estimates obtained in Part 3 (in STATA). 
 ####
 
 ####
